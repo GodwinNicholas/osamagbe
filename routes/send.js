eb4490b7c83546da98bf6e628b4e15a061e6a2a2
Godwin Nicholas <iamgodwin@gmail.com>
2019-11-08 11:00:28 +0100

    added new feature

--- routes/send.js
+++ routes/send.js
@@ -8,25 +8,9 @@ router.get("/", ensureAuthenticated, (req, res) => {
 });
 
 router.post("/", (req, res) => {
-    let counter = 0;
     Email.find({ user: req.user })
         .then((emailList) => {
-            emailList.forEach(e => {
-                (async function send() {
-                    if (counter < emailList.length) {
-                        await setTimeout(() => {
-                            sendmail(e.address, req.body.subject, req.body.body);
-                            counter++;
-                            console.log(counter)
-                            return send()
-                        }, 30000);
-                    }
-                    else {
-                        console.log("done")
-                        return res.redirect("/");
-                    }
-                })()
-            });
+            sendmail(e.address, req.body.subject, req.body.body, emailList);
             return res.redirect("/");
         })
 });
--- utils/sendmail.js
+++ utils/sendmail.js
@@ -1,30 +1,86 @@
 const nodemailer = require("nodemailer");
+const Email = require("../models/Email");
 
-function sendmail(receiver, subject, html) {
-    var transporter = nodemailer.createTransport({
-        service: 'gmail',
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        pool: true, //
-        auth: {
-            user: 'avery77bradley@gmail.com', // here use your real email
-            pass: 'godwinemail123' // put your password correctly (not in this question please)
+function sendmail(subject, html, list) {
+    let counter = list.length;
+    let email_counter;
+    const emails = [
+        {
+            address: "donaldcris176@gmail.com",
+            password: "godwinemail123"
+        },
+        {
+            address: "wadefumi@gmail.com",
+            password: "osamagbe123"
+        },
+        {
+            address: "harikuwade@gmail.com",
+            password: "osamagbe123"
+        },
+        {
+            address: "Grace7sure@gmail.com",
+            password: "osamagbe123"
+        },
+        {
+            address: "laurajo77hnson@gmail.com",
+            password: "osamagbe123"
         }
-    });
+    ];
 
-    var message = {
-        from: "avery77bradley@gmail.com",
-        to: receiver,
-        subject: subject,
-        html: html
-    };
 
-    transporter.sendMail(message, (err, info) => {
-        console.log('err', err);
-        console.log("info", info);
-        return 1;
-    })
+    // call initial
+    send(emails[email_counter].address, emails[email_counter].password, counter);
+
+    function send(address, password, counter, email_counter) {
+        var transporter = nodemailer.createTransport({
+            service: 'gmail',
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
+            pool: true, //
+            auth: {
+                user: address, // here use your real email
+                pass: password // put your password correctly (not in this question please)
+            }
+        });
+
+        var message = {
+            from: address,
+            to: list[counter],
+            subject: subject,
+            html: html
+        };
+
+        transporter.sendMail(message, (err, info) => {
+            if (err) {
+                console.log(err);
+                counter--;
+                email_counter++;
+                if (counter != 0) {
+                    transporter.close();
+                    return send(emails[email_counter].address, emails[email_counter].password, counter, email_counter);
+                }
+                console.log("done....")
+            }
+            else {
+                Email.deleteOne({ address })
+                    .then(() => {
+                        if (email_counter > 4) {
+                            email_counter = 0;
+                        }
+                        console.log(`message sent ${info}`)
+                        counter--;
+                        email_counter++;
+                        if (counter != 0) {
+                            transporter.close();
+                            return send(emails[email_counter].address, emails[email_counter].password, counter, email_counter);
+                        }
+                        console.log("done....")
+                        return
+                    })
+            }
+        })
+    }
 }
 
 module.exports = sendmail;
